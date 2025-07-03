2e0672485e3d60e39843ad6b1bcad030036320a6
Umar Aslam <umeraslam@greeka.com>
2025-07-04 00:51:59 +0500

    fix: fix redirect URI for vercel

--- components/ActionButtons/GetAllSheetsButton.tsx
+++ components/ActionButtons/GetAllSheetsButton.tsx
@@ -6,7 +6,8 @@ const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?` +
     client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
     redirect_uri: isLocalhost
       ? 'http://localhost:3000/api/google/callback'
-      : 'https://sheet-sync-hub-c4pdfjzmi-umerheres-projects.vercel.app/api/google/callback',
+      : 'https://sheet-sync-hub-app.vercel.app/api/google/callback',
+      
     response_type: 'code',
     access_type: 'offline',
     prompt: 'consent',
