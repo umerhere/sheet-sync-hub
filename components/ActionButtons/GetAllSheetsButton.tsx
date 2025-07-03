75cccb1e586b5db124947d2eb11d4af6b30e5061
Umar Aslam <umeraslam@greeka.com>
2025-07-04 01:11:18 +0500

    fix: env variable names

--- components/ActionButtons/GetAllSheetsButton.tsx
+++ components/ActionButtons/GetAllSheetsButton.tsx
@@ -2,7 +2,7 @@
 
 const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?` +
   new URLSearchParams({
-    client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+    client_id: process.env.GOOGLE_CLIENT_ID!,
     // redirect_uri: 'http://localhost:3000/api/google/callback',
     redirect_uri: `${window.location.origin}/api/google/callback`,
     response_type: 'code',
