b7dd4a80bd64389bcce2f3edb94873715288b971
Umar Aslam <umeraslam@greeka.com>
2025-07-04 00:48:22 +0500

    fix: fix redirect URI for google

--- components/ActionButtons/GetAllSheetsButton.tsx
+++ components/ActionButtons/GetAllSheetsButton.tsx
@@ -1,9 +1,12 @@
 'use client'
+const isLocalhost = typeof window !== 'undefined' && window.location.hostname === 'localhost'
 
 const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?` +
   new URLSearchParams({
     client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-    redirect_uri: 'http://localhost:3000/api/google/callback',
+    redirect_uri: isLocalhost
+      ? 'http://localhost:3000/api/google/callback'
+      : 'https://sheet-sync-hub-c4pdfjzmi-umerheres-projects.vercel.app/api/google/callback',
     response_type: 'code',
     access_type: 'offline',
     prompt: 'consent',
