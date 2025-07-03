a31877f07491c30102ce12d2f4ea9184fd078a0f
Umar Aslam <umeraslam@greeka.com>
2025-07-04 00:07:11 +0500

    feat: add custom notFind fallback

---
+++ app/app/not-found.tsx
@@ -0,0 +1,12 @@
+// app/not-found.tsx
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+        <p className="text-gray-500">The page you are looking for does not exist.</p>
+      </div>
+    </div>
+  );
+}
