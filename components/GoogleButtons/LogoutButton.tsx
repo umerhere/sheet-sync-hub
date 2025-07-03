db47408ef1d830eed4911d5cebc1e29760f5bd7d
Umar Aslam <umeraslam@greeka.com>
2025-07-04 00:30:08 +0500

    fix: resolve env variable prefix

--- components/GoogleButtons/LogoutButton.tsx
+++ components/GoogleButtons/LogoutButton.tsx
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation"
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
 
 export default function LogoutButton() {
