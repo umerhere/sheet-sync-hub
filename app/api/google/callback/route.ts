e61e0cf71792f232c1cad0089ee944e2d7ff498f
Umar Aslam <umeraslam@greeka.com>
2025-07-04 01:15:26 +0500

    fix: env variable names make public

--- app/api/google/callback/route.ts
+++ app/api/google/callback/route.ts
@@ -34,8 +34,8 @@ export async function GET (req: NextRequest) {
       },
       body: new URLSearchParams({
         code,
-        client_id: process.env.GOOGLE_CLIENT_ID!,
-        client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+        client_id: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+        client_secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
         // redirect_uri: 'http://localhost:3000/api/google/callback',
         redirect_uri: new URL('/api/google/callback', req.url).toString(),
         grant_type: 'authorization_code'
